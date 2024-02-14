325de86abb88e3b68bdff31ba113de80e97210f3
cliffmanw <96119888+cliffmanw@users.noreply.github.com>
2024-02-14 09:31:35 +0800

    Update diy-part2-ss.sh

--- diy-part2-ss.sh
+++ diy-part2-ss.sh
@@ -9,7 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# Modify default IP cliffman
+# Modify default IP cliffman moded
 # sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='OP'/g" package/base-files/files/bin/config_generate
 
@@ -17,7 +17,7 @@ sed -i "s/hostname='.*'/hostname='OP'/g" package/base-files/files/bin/config_gen
 #git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-# cliffman mod
+# cliffman moded
 # git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 # git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 
@@ -27,11 +27,12 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-# themes添加（svn co 命令意思：指定版本如https://github）
+# themes添加（svn co 命令意思：指定版本如https://github）cliffman moded
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 # git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+#git clone https://github.com/jerrykuku/luci-theme-argon
 
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 
